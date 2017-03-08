ee8cbd2357e4e2d7925676de0e283e7e05b72adb
kaileighrose <kaileighrose@gmail.com>
2017-03-08 13:38:48 -0500

    setup new project and initial method

---
+++ src/com/tutorials/calcengine/Main.java
@@ -0,0 +1,24 @@
+package com.tutorials.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double val1 = 100.0d;
+	double val2 = 50.0d;
+	double result;
+	char opCode = 'a';
+
+	if(opCode == 'a')
+	    result = val1 + val2;
+	else if(opCode == 's')
+        result = val1 - val2;
+	else if(opCode == 'd')
+        result = val1 / val2;
+	else if(opCode == 'm')
+        result = val1 * val2;
+	else
+	    result = 0.0d;
+
+	    System.out.println(result);
+    }
+}
